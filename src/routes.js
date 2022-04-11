9c962ecf9a37189731641bd7ede4d74dea5fc3ea
Lucas Almeida <101877534+Lucas-RS-Almeida@users.noreply.github.com>
2022-04-11 07:07:10 -0300

    Fix for user security

--- src/routes.js
+++ src/routes.js
@@ -15,15 +15,15 @@ routes.post('/auth/sign-up', AuthController.signUp);
 routes.post('/auth/log-in', AuthController.logIn);
 
 // Routes to user
-routes.put('/users/change-email/:id', auth, UserController.changeEmail);
-routes.put('/users/change-password/:id', auth, UserController.changePassword);
+routes.put('/users/change-email', auth, UserController.changeEmail);
+routes.put('/users/change-password', auth, UserController.changePassword);
 routes.post('/users/forgot-password', UserController.forgotPassword);
 routes.post('/users/reset-password', UserController.resetPassword);
-routes.delete('/users/:id', auth, UserController.delete);
+routes.delete('/users', auth, UserController.delete);
 
 // Routes to category
 routes.post('/categories', auth, isAdmin, CategoryController.store);
-routes.get('/categories', auth, CategoryController.index);
+routes.get('/categories', auth, isAdmin, CategoryController.index);
 routes.get('/categories/:id', auth, isAdmin, CategoryController.show);
 routes.put('/categories/:id', auth, isAdmin, CategoryController.update);
 routes.delete('/categories/:id', auth, isAdmin, CategoryController.delete);
